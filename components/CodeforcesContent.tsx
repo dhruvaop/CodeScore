1a439b5d2c05db7eb3fc6d7edcdce1a172f5d68e
Dhruva Bhattacharya <71749153+dhruvaop@users.noreply.github.com>
2024-06-20 03:18:09 +0530

    Update CodeforcesContent.tsx

--- components/CodeforcesContent.tsx
+++ components/CodeforcesContent.tsx
@@ -190,7 +190,7 @@ function CodeforcesContent(): JSX.Element {
               href="https://www.linkedin.com/in/dhruva-bhattacharya/"
               target="_blank"
               rel="noopener noreferrer"
-              className={classes.link}
+              //className={classes.link}
             >
              Dhruva Bhattacharya with ❤️
             </a>
